bc731149c17abf95470446c3a73df1daa2f64e30
Bruno Artur <batlopes@yahoo.com.br>
2022-06-13 23:51:24 -0300

    Adding infinity loop to main

--- src/main.py
+++ src/main.py
@@ -7,11 +7,15 @@ if __name__ == '__main__':
     plateau_x = int(plateau_dimensions[0])
     plateau_y = int(plateau_dimensions[1])
     plateau = Plateau(plateau_x, plateau_y)
-    rover_position = input('Digite a posição e a direção iniciais do rover: ').split(' ')
-    rover_x = int(rover_position[0])
-    rover_y = int(rover_position[1])
-    rover_direction = rover_position[2]
-    rover = Rover(rover_x, rover_y, rover_direction, plateau)
-    rover_commands = input('Digite os comandos do rover: ')
-    rover.navigate(rover_commands)
-    print(f'Rover final position: {rover.position_x} {rover.position_y} {rover.direction}')
+    while True:
+        rover_position = input('Digite a posição e a direção iniciais do rover: ').split(' ')
+        if rover_position[0].lower() == 'exit':
+            break
+        rover_x = int(rover_position[0])
+        rover_y = int(rover_position[1])
+        rover_direction = rover_position[2].upper()
+        rover = Rover(rover_x, rover_y, rover_direction, plateau)
+        rover_commands = input('Digite os comandos do rover: ')
+        rover.navigate(rover_commands)
+        print(f'Posição final: {rover.position_x} {rover.position_y} {rover.direction}')
+        print('-----------------------------------------------------')
